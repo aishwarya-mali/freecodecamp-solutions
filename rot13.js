56045cfdf7d37eb6cbf0d7bb4fecebf6294a9724
aishwarya-mali <aishwaryamali29@gmail.com>
2023-03-20 12:59:20 +0530

    alternate solution

--- rot13.js
+++ rot13.js
@@ -19,3 +19,23 @@ function rot13(str) {
   }
   
   rot13("SERR CVMMN!");
+
+  //SOLUTION using charCodeAt() & String.fromCharCode()
+
+  function rot13(str) {
+    let decodedStr = ''
+    for (let i = 0; i < str.length; i++) {
+      let asciiNum = str[i].charCodeAt()
+      if(asciiNum >= 65 && asciiNum <= 77){
+        decodedStr += String.fromCharCode(asciiNum + 13)
+      }else if(asciiNum >= 78 && asciiNum <= 90){
+        decodedStr += String.fromCharCode(asciiNum - 13)
+      }else{
+        decodedStr += str[i]
+      }
+    }
+    console.log(decodedStr)
+    return decodedStr;
+  }
+  
+  rot13("SERR CVMMN!");
