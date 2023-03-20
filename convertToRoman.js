4564252fc582dc70410f14cb0133dcb72b334002
aishwarya-mali <aishwaryamali29@gmail.com>
2023-03-20 11:58:54 +0530

    code formatting

--- convertToRoman.js
+++ convertToRoman.js
@@ -1,33 +1,33 @@
 function convertToRoman(num) {
     if (!(typeof num === 'number' && num > 0 && num <= 3999)) {
-      return 'Please enter positive number which is less than 3999'
+        return 'Please enter positive number which is less than 3999'
     }
-  
+
     const romanToNum = {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
     }
-    
+
     let romanNum = ''
     for (const key in romanToNum) {
-      while (num >= romanToNum[key]) {
-        romanNum += key
-        num -= romanToNum[key]
-      }
+        while (num >= romanToNum[key]) {
+            romanNum += key
+            num -= romanToNum[key]
+        }
     }
-    
+
     return romanNum;
-  }
-  
-  convertToRoman(16);
+}
+
+convertToRoman(16);
