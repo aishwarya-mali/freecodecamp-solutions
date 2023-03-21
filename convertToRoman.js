5eafc6833bef0b2dc9e4cdbf66c87428adc2ab84
aishwarya-mali <aishwaryamali29@gmail.com>
2023-03-21 17:32:37 +0530

    add all problem statements

--- convertToRoman.js
+++ convertToRoman.js
@@ -1,3 +1,22 @@
+// Roman Numeral Converter
+// Convert the given number into a roman numeral.
+
+// Roman numerals	Arabic numerals
+// M	1000
+// CM	900
+// D	500
+// CD	400
+// C	100
+// XC	90
+// L	50
+// XL	40
+// X	10
+// IX	9
+// V	5
+// IV	4
+// I	1
+// All roman numerals answers should be provided in upper-case.
+
 function convertToRoman(num) {
     if (!(typeof num === 'number' && num > 0 && num <= 3999)) {
         return 'Please enter positive number which is less than 3999'
